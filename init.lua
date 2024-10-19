921f699fa3321d6c10543c72b107405728c6ab20
Jim Fitzpatrick <jfitzpat@redhat.com>
2024-10-19 21:40:20 +0100

    ADD: harpoon
    This is a plugin that I want to use but for some reason it is not
    loading correctly
    
    Signed-off-by: Jim Fitzpatrick <jfitzpat@redhat.com>

--- init.lua
+++ init.lua
@@ -851,6 +851,9 @@ require('lazy').setup {
   require 'plugins.twilight',
   require 'plugins.zenmode',
 
+  -- plugins that currently does not work
+  -- require 'plugins.harpoon',
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
